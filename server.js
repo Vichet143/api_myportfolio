33fd9bff002080e006662d1feda02f08af39d584
Vichet143 <zhongfyer@gmail.com>
2026-06-24 19:38:49 +0700

    change port

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(ExperienceRoute);
 app.use(CertificateRoute);
 app.use(SoftSkillRoute)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
