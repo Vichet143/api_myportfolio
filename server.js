d9ce41ec566e6c00905ec20771c5042ec124798b
Vichet143 <zhongfyer@gmail.com>
2026-06-24 19:33:17 +0700

    edit page server

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(CertificateRoute);
 app.use(SoftSkillRoute)
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
