a1cf5008d96769f9193bd9230872d62c7ce09fa1
Vichet143 <zhongfyer@gmail.com>
2026-06-24 19:56:44 +0700

    change config

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,8 @@
 import admin from "firebase-admin";
 import serviceAccount from "./portfolio-ccfaa-firebase-adminsdk-fbsvc-21593ef2bc.json" with { type: "json" };
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://portfolio-ccfaa-default-rtdb.asia-southeast1.firebasedatabase.app"
